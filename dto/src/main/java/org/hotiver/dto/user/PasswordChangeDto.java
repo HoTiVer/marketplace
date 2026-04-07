f583fa636b3383f3fd7b1d0c9d75bb2d43f7feef
hotiver <108735602+HoTiVer@users.noreply.github.com>
2026-04-07 13:26:42 +0300

    add verification in PasswordChangeDto

--- dto/src/main/java/org/hotiver/dto/user/PasswordChangeDto.java
+++ dto/src/main/java/org/hotiver/dto/user/PasswordChangeDto.java
@@ -1,6 +1,7 @@
 package org.hotiver.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,11 @@ public class PasswordChangeDto {
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Pattern(
+            regexp = "^[A-Za-z0-9!@#$%^&*()_+=\\-]+$",
+            message = "Password can contain only English letters, digits and allowed symbols"
+    )
+
     private String newPassword;
 
     private String code;
